c6b691dc0181fe8693dd60820a4306191cd08c42
gauravyadv <gauravyadavry_cse18@its.edu.in>
2024-03-27 09:14:24 +0530

    basic server setup and folder setup

---
+++ server/src/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 12000; 
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
